e59db9f5e34b6f95dac2d3b5f7e6a7d62de03ae7
mederic mesle <mederic.mesle@gmail.com>
2022-04-16 21:10:21 +0200

    respond_to?

--- Part 1. Ruby foundations/Chapter 2. Objects, methods, and local variables/ticket.rb
+++ Part 1. Ruby foundations/Chapter 2. Objects, methods, and local variables/ticket.rb
@@ -28,3 +28,12 @@ puts "This ticket is for: #{ticket.event}, at #{ticket.venue}.\n" +
 "The performer is #{ticket.performer}.\n" +
 "The seat is #{ticket.seat}, " +
 "and it costs $#{"%.2f." % ticket.price}"
+
+print "Information desired: "
+request = gets.chomp
+
+if ticket.respond_to?(request)
+  puts ticket.send(request)
+else
+  puts "No such information available"
+end
