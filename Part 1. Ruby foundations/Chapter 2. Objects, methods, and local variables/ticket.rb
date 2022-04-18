6ebe342b7e7ac946fd3ae1f92eb32423441e9d8b
mederic mesle <mederic.mesle@gmail.com>
2022-04-18 14:30:38 +0200

    Ticket

--- Part 1. Ruby foundations/Chapter 2. Objects, methods, and local variables/ticket.rb
+++ Part 1. Ruby foundations/Chapter 2. Objects, methods, and local variables/ticket.rb
@@ -24,11 +24,17 @@ def ticket.price
   5.50
 end
 
+def ticket.print_details(*x)
+  x.each { |detail| puts "This ticket is #{detail}"}
+end
+
 puts "This ticket is for: #{ticket.event}, at #{ticket.venue}.\n" +
 "The performer is #{ticket.performer}.\n" +
 "The seat is #{ticket.seat}, " +
 "and it costs $#{"%.2f." % ticket.price}"
-
+ticket.print_details("non-refundable.",
+  "non-transferable.",
+  "in a non-smoking section.")
 print "Information desired: "
 request = gets.chomp
 
